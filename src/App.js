a053b8fa8d3cc88dbd4a08a9c7047681dd19c407
bassem eloraby <95348711+bassemeloraby@users.noreply.github.com>
2022-10-20 00:35:31 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Head from "./Head";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Head />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
